e20ff0ab011d8abb44c9cbcad4e5e39d00406c82
ArisaOkuyama <arisaokuyama@gmail.com>
2020-08-11 14:02:18 +1000

    Updates

---
+++ precache-manifest.57084ad0ef7a41b3405f981fe53d8789.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed26bed3a79f3a785c5dca3f1d687e1",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "acccfbc4c754d278c34c",
+    "url": "/react_portfolio/static/css/main.4b8d3ab5.chunk.css"
+  },
+  {
+    "revision": "f925a95b94d12b648fc9",
+    "url": "/react_portfolio/static/js/2.6487f767.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_portfolio/static/js/2.6487f767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acccfbc4c754d278c34c",
+    "url": "/react_portfolio/static/js/main.81df425c.chunk.js"
+  },
+  {
+    "revision": "a06b7793b710388c05e6",
+    "url": "/react_portfolio/static/js/runtime-main.b51f7317.js"
+  },
+  {
+    "revision": "d9fbbd4f730415243daf25ecc396dc26",
+    "url": "/react_portfolio/static/media/ecoanew.d9fbbd4f.png"
+  },
+  {
+    "revision": "0f52a9c5e0d665174b5cb4ea2f7d927e",
+    "url": "/react_portfolio/static/media/firstportfolio.0f52a9c5.png"
+  },
+  {
+    "revision": "e02519f93daba778e752154d73b30561",
+    "url": "/react_portfolio/static/media/marketplace.e02519f9.png"
+  },
+  {
+    "revision": "e5aeece0b9f3cd172661e507819f7e2c",
+    "url": "/react_portfolio/static/media/meandlake.e5aeece0.jpg"
+  },
+  {
+    "revision": "eca8374118d023fe8b0cf81b5cdd875f",
+    "url": "/react_portfolio/static/media/profilephoto.eca83741.jpg"
+  }
+]);
