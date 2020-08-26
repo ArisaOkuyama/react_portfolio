8c379671a46f6e429215f20c16214de71c6bd624
ArisaOkuyama <arisaokuyama@gmail.com>
2020-08-26 10:09:56 +1000

    changed profile pic

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -23,7 +23,7 @@ const AboutMe = () => {
                     <h2>{store.language ? "ABOUT ME" : "自己紹介"}</h2>
                     <div className="image-deco">
                         <div className="deco"></div>
-                        <img className="profile-photo" alt="me" src={ require('../img/profilephoto.jpg') } />
+                        <img className="profile-photo" alt="me" src="https://media-exp1.licdn.com/dms/image/C4D03AQGR51TVKQnN5w/profile-displayphoto-shrink_400_400/0?e=1603929600&v=beta&t=7AEyV5GR9UFsnzMT8fT3U1hd_ru2H2PMNluygzzLtzs" />
                     </div>
                 </div>
                 <div className="aboutme-body">
